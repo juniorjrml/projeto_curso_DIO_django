33fe5a03e5cef7a74fc229c3de7078094eaf774b
juniorjrml <jancarlolcj@gmail.com>
2020-12-07 16:03:14 -0300

    adicionando autenticacao e logout

--- core/views.py
+++ core/views.py
@@ -1,18 +1,43 @@
 from django.shortcuts import render, HttpResponse, redirect
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 from core.models import Evento
 
-
-def hello(request, nome, idade):
-    return HttpResponse('Hello Word {}! Voce tem {} anos!'.format(nome, idade))
 
 def local(request, evento):
     return HttpResponse('Hello Word {}! Voce tem {} anos!')
 
+@login_required(login_url='/login')
 def lista_eventos(request):
-    evento = Evento.objects.all()
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
+
+def login_user(request):
+    return render(request, "login.html")
+
+
+def submit_login(request):
+    if request.POST:
+        nome = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+        usuario = authenticate(username=nome, password=senha)
+        print(usuario)
+        if usuario:
+            login(request, usuario)
+
+        else:
+            messages.error(request, "Usuario ou Senha Invalidos")  #  retorna em caso da autenticação falhar
+
+
+    return redirect('/')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
 
 def index(request):
     return redirect('/agenda/')
--- projeto_curso_dio_django/urls.py
+++ projeto_curso_dio_django/urls.py
@@ -20,7 +20,9 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/<nome>/<int:idade>', views.hello),
     path('agenda/', views.lista_eventos),
-    path('', RedirectView.as_view(url='/agenda/'))
+    path('login/', views.login_user),
+    path('', RedirectView.as_view(url='/agenda/')),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user)
 ]
